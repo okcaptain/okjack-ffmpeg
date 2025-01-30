563cd7aea4b00ff02321996a93c440a9b75e934c
okjack <okjackcaptain@gmail.com>
2025-01-30 09:43:12 +0800

    av3a

--- libavutil/channel_layout.c
+++ libavutil/channel_layout.c
@@ -197,11 +197,21 @@ static const struct channel_layout_name channel_layout_map[] = {
     { "7.1(wide)",      AV_CHANNEL_LAYOUT_7POINT1_WIDE_BACK   },
     { "7.1(wide-side)", AV_CHANNEL_LAYOUT_7POINT1_WIDE        },
     { "7.1(top)",       AV_CHANNEL_LAYOUT_7POINT1_TOP_BACK    },
+    { "5.1.2",          AV_CHANNEL_LAYOUT_5POINT1POINT2_BACK  },
     { "octagonal",      AV_CHANNEL_LAYOUT_OCTAGONAL           },
     { "cube",           AV_CHANNEL_LAYOUT_CUBE                },
+    { "5.1.4",          AV_CHANNEL_LAYOUT_5POINT1POINT4_BACK  },
+    { "7.1.2",          AV_CHANNEL_LAYOUT_7POINT1POINT2       },
+    { "7.1.4",          AV_CHANNEL_LAYOUT_7POINT1POINT4_BACK  },
     { "hexadecagonal",  AV_CHANNEL_LAYOUT_HEXADECAGONAL       },
     { "downmix",        AV_CHANNEL_LAYOUT_STEREO_DOWNMIX,     },
     { "22.2",           AV_CHANNEL_LAYOUT_22POINT2,           },
+    { "10.2",         	AV_CHANNEL_LAYOUT_10POINT2 },
+    { "5.1.2",       	AV_CHANNEL_LAYOUT_5POINT1POINT2 },
+    { "5.1.4",       	AV_CHANNEL_LAYOUT_5POINT1POINT4 },
+    { "7.1.2",       	AV_CHANNEL_LAYOUT_7POINT1POINT2 },
+    { "7.1.4",       	AV_CHANNEL_LAYOUT_7POINT1POINT4_BACK },
+    { "hoa2(9.0)",   	AV_CHANNEL_LAYOUT_9POINT0 },
 };
 
 #if FF_API_OLD_CHANNEL_LAYOUT
