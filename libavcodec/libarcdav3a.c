d6ec84872a6a3110a02952cb74b6b68b69d2b728
okjack <okjackcaptain@gmail.com>
2025-01-31 09:57:54 +0800

    Update libarcdav3a.c

--- libavcodec/libarcdav3a.c
+++ libavcodec/libarcdav3a.c
@@ -6,6 +6,7 @@
 #include "decode.h"
 #include "avcodec.h"
 #include "internal.h"
+#include "libavutil/opt.h"
 #include "libavutil/mastering_display_metadata.h"
 #if ARCH_AARCH64
 #include <arm_neon.h>
