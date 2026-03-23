9c19a3ec23923032e54e877820badafdb5c1a462
okjack <okjackcaptain@gmail.com>
2026-03-23 16:49:17 +0800

    libavformat/hls.c: update allowed_segment_extensions update hls_probe

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -2763,8 +2763,8 @@ static int hls_probe(const AVProbeData *p)
 {
     /* Require #EXTM3U at the start, and either one of the ones below
      * somewhere for a proper match. */
-    if (strncmp(p->buf, "#EXTM3U", 7))
-        return 0;
+    if (!strncmp(p->buf, "#EXTM3U", 7))
+        return AVPROBE_SCORE_MAX;
 
     if (strstr(p->buf, "#EXT-X-STREAM-INF:")     ||
         strstr(p->buf, "#EXT-X-TARGETDURATION:") ||
@@ -2812,7 +2812,7 @@ static const AVOption hls_options[] = {
         INT_MIN, INT_MAX, FLAGS},
     {"allowed_segment_extensions", "List of file extensions that hls is allowed to access",
         OFFSET(allowed_segment_extensions), AV_OPT_TYPE_STRING,
-        {.str = "3gp,aac,avi,ac3,eac3,flac,mkv,m3u8,m4a,m4s,m4v,mpg,mov,mp2,mp3,mp4,mpeg,mpegts,ogg,ogv,oga,ts,vob,vtt,wav,webvtt"
+        {.str = "ALL"
             ",cmfv,cmfa" // Ticket11526 www.nicovideo.jp
             ",ec3"       // part of Ticket11435 (Elisa Viihde (Finnish online recording service))
             ",fmp4"      // https://github.com/yt-dlp/yt-dlp/issues/12700
